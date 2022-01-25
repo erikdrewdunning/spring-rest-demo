a0cd4ec959b90e9acb2c3e4bfb2005d80e405084
erikdrewdunning <erikdunning@icloud.com>
2022-01-25 11:07:08 -0700

    Add links

--- src/main/java/com/erikdunning/SpringRestDemoApplication.java
+++ src/main/java/com/erikdunning/SpringRestDemoApplication.java
@@ -3,11 +3,12 @@ package com.erikdunning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import com.erikdunning.payroll.PayrollApplication;
+
 public class SpringRestDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringRestDemoApplication.class, args);
+		SpringApplication.run(PayrollApplication.class, args);
 	}
 
 }
