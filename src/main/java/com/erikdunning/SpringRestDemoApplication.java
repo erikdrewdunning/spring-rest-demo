4d0c2e67cc069d5f39e1c36781fefbefe78f3e76
erikdrewdunning <erikdunning@icloud.com>
2022-01-25 11:14:47 -0700

    Add more links

--- src/main/java/com/erikdunning/SpringRestDemoApplication.java
+++ src/main/java/com/erikdunning/SpringRestDemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.erikdunning.payroll.PayrollApplication;
 
+//@SpringBootApplication
 public class SpringRestDemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/erikdunning/payroll/EmployeeController.java
+++ src/main/java/com/erikdunning/payroll/EmployeeController.java
@@ -1,7 +1,12 @@
 package com.erikdunning.payroll;
 
-import java.util.List;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,61 +15,61 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 class EmployeeController {
 
-  private final EmployeeRepository repository;
+	private final EmployeeRepository repository;
 
-  EmployeeController(EmployeeRepository repository) {
-    this.repository = repository;
-  }
+	EmployeeController(EmployeeRepository repository) {
+		this.repository = repository;
+	}
 
+	@GetMapping("/employees")
+	CollectionModel<EntityModel<Employee>> all() {
 
-  // Aggregate root
-  // tag::get-aggregate-root[]
-  @GetMapping("/employees")
-  List<Employee> all() {
-    return repository.findAll();
-  }
-  // end::get-aggregate-root[]
+		List<EntityModel<Employee>> employees = repository.findAll().stream()
+				.map(employee -> EntityModel.of(employee,
+						linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+						linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+				.collect(Collectors.toList());
 
-  @PostMapping("/employees")
-  Employee newEmployee(@RequestBody Employee newEmployee) {
-    return repository.save(newEmployee);
-  }
+		return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
+	}
 
-  // Single item
-  
-  @GetMapping("/employees/{id}")
-  EntityModel<Employee> one(@PathVariable Long id) {
+	@PostMapping("/employees")
+	Employee newEmployee(@RequestBody Employee newEmployee) {
+		return repository.save(newEmployee);
+	}
 
-    Employee employee = repository.findById(id) //
-        .orElseThrow(() -> new EmployeeNotFoundException(id));
+	// Single item
 
-    return EntityModel.of(employee, //
-        linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-        linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
-  }
+	@GetMapping("/employees/{id}")
+	EntityModel<Employee> one(@PathVariable Long id) {
 
-  @PutMapping("/employees/{id}")
-  Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
-    
-    return repository.findById(id)
-      .map(employee -> {
-        employee.setName(newEmployee.getName());
-        employee.setRole(newEmployee.getRole());
-        return repository.save(employee);
-      })
-      .orElseGet(() -> {
-        newEmployee.setId(id);
-        return repository.save(newEmployee);
-      });
-  }
+		Employee employee = repository.findById(id) //
+				.orElseThrow(() -> new EmployeeNotFoundException(id));
 
-  @DeleteMapping("/employees/{id}")
-  void deleteEmployee(@PathVariable Long id) {
-    repository.deleteById(id);
-  }
+		return EntityModel.of(employee, //
+				linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+				linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+	}
+
+	@PutMapping("/employees/{id}")
+	Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+
+		return repository.findById(id).map(employee -> {
+			employee.setName(newEmployee.getName());
+			employee.setRole(newEmployee.getRole());
+			return repository.save(employee);
+		}).orElseGet(() -> {
+			newEmployee.setId(id);
+			return repository.save(newEmployee);
+		});
+	}
+
+	@DeleteMapping("/employees/{id}")
+	void deleteEmployee(@PathVariable Long id) {
+		repository.deleteById(id);
+	}
 }
